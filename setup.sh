b3d2bdfbd78b9681aa354f065213849577b1a92c
Ivan Gao <ivankz@163.com>
2024-01-16 15:42:37 +0800

    refactor: using cnosdb latest.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 docker-compose up -d  meta1 meta2 meta3
+sleep 1
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/init -d '{}'
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/add-learner -H "Content-Type: application/json" -d '[2, "meta2.cnosdb.com:8901"]'
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/add-learner -H "Content-Type: application/json" -d '[3, "meta3.cnosdb.com:8901"]'

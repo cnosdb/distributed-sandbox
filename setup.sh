6ec73e6d495962d4985898870079b361a3d22f0f
Ivan Gao <ivankz@163.com>
2024-08-27 17:48:30 +0800

    feat: community config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/add-learner -H
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/add-learner -H "Content-Type: application/json" -d '[3, "meta3.cnosdb.com:8901"]'
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/change-membership -H "Content-Type: application/json" -d '[1,2,3]'
 sleep 2
-docker-compose up -d query_tskv1 query_tskv2
+docker-compose up -d query_tskv1 query_tskv2 query_tskv2
 sleep 2
 docker-compose up -d nginx

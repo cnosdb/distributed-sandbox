df53b6185576552fa207c7040726acdbb2d11911
Ivan Gao <92921813+IvanGao01@users.noreply.github.com>
2024-09-27 16:34:34 +0800

    feat: update docker compose file. (#2)

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 docker-compose up -d  meta1 meta2 meta3
+sleep 2
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/init -d '{}'
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/add-learner -H "Content-Type: application/json" -d '[2, "meta2.cnosdb.com:8901"]'
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/add-learner -H "Content-Type: application/json" -d '[3, "meta3.cnosdb.com:8901"]'
 docker exec meta1.cnosdb.com curl -s http://meta1.cnosdb.com:8901/change-membership -H "Content-Type: application/json" -d '[1,2,3]'
-sleep 2
-docker-compose up -d query_tskv1 query_tskv2
-sleep 2
+# sleep 2
+docker-compose up -d query_tskv1 query_tskv2 query_tskv3
+# sleep 2
 docker-compose up -d nginx
